0e50ce575474a91e6b7be8eae89f339598910739
tomkjose <tomjosekunnumpurath@gmail.com>
2023-08-28 08:20:11 +0530

    new commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://gregarious-crepe-c1ca37.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
